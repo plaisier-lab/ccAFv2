b59bc733eaabccc67bc8d6889d90539663a13948
Samantha O'Connor <saoconn1@asu.edu>
2024-04-10 14:46:18 -0700

    updates to data and scripts

--- scripts/09_plotting_CV_classification_methods.py
+++ scripts/09_plotting_CV_classification_methods.py
@@ -17,7 +17,7 @@
 ## mention who built it. Thanks. :-)                    ##
 ##########################################################
 
-#docker run -it -v '/home/soconnor/old_home/ccNN:/files' cplaisier/ccnn
+#docker run -it -v '/home/soconnor/old_home/ccNN/ccAFv2:/files' cplaisier/ccnn
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -36,18 +36,15 @@ from scipy import stats
 
 data1 = 'U5'
 tags = ['SVMrej', 'RFpy', 'KNN', 'ACTINN','ccAFv2']
-resdir = 'results/classification_method_comparison_redo_with_10_perc_holdout'
-savedir = 'plots'
+resdir = 'compare_classification_methods'
 toPlot = 'f1-score'
 
 ## Load datasets and concatenate
 cReport = {}
 for tag1 in tags:
-    if tag1 == 'ccAFv2':
-        tmp1 = pd.read_csv(resdir+'/'+tag1+'_CV_classification_report_861_U5_102423_fc_25_5_layers_600_200.csv')
-    else:
-        tmp1 = pd.read_csv(resdir+'/'+tag1+'_CV_classification_report_861.csv')
-    cReport[tag1] = tmp1
+    resdir2 = resdir+'/'+tag1
+    datas = pd.read_csv(resdir2+'/'+tag1+'_CV_classification_report_861_'+data1+'.csv')
+    cReport[tag1] = datas
 
 cReports = pd.concat([cReport[tags[0]], cReport[tags[1]], cReport[tags[2]], cReport[tags[3]], cReport[tags[4]]], axis = 0)
 cReports.rename(columns={'Unnamed: 0':'Cell Cycle State'}, inplace=True)
@@ -68,9 +65,8 @@ fig, ax = plt.subplots(figsize=(40,20))
 sns.boxplot(hue="Classifier", y=toPlot, x = "Cell Cycle State", data=cReports, linewidth=1, order=['Neural G0', 'G1', 'Late G1', 'S', 'S/G2', 'G2/M', 'M/Early G1'])
 ax.set_ylim(0,1)
 ax.set(ylabel=toPlot)
-plt.savefig(resdir+'/'+data1+'_'+toPlot+'_030724.pdf')
+plt.savefig(resdir+'/'+data1+'_'+toPlot+'.pdf')
 plt.clf()
-
 
 # stats
 stats = {}
@@ -80,11 +76,10 @@ for classifier1 in ['SVMrej', 'RFpy', 'KNN', 'ACTINN']:
     tmp = cReports[cReports['Classifier'] == classifier1]
     ref = cReports[cReports['Classifier'] == 'ccAFv2']
     for state1 in tmp['Cell Cycle State'].unique():
-        print(state1)
         comp = tmp[tmp['Cell Cycle State'] == state1]['f1-score']
         ccafv2 = ref[ref['Cell Cycle State'] == state1]['f1-score']
         stats[classifier1][state1] = scipy.stats.ttest_ind(comp,ccafv2)[1]
 
 
 df = pd.DataFrame(stats)
-df.to_csv(resdir+'/ccAFv2_significantly_different_for_each_classifier_for_each_state_stats.csv')
+df.to_csv(resdir+'/'+data1+'_ccAFv2_stats_against_other_classification_methods.csv')
