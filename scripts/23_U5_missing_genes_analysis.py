9691a58086ba3ba4a4a9a1211ed1de1d7cdac98e
Samantha O'Connor <saoconn1@asu.edu>
2024-04-11 10:59:14 -0700

    editing scripts

--- scripts/23_U5_missing_genes_analysis.py
+++ scripts/23_U5_missing_genes_analysis.py
@@ -17,7 +17,7 @@
 ## mention who built it. Thanks. :-)                    ##
 ##########################################################
 
-#docker run -it -v '/home/soconnor/old_home:/files' cplaisier/ccnn
+#docker run -it -v '/home/soconnor/old_home/ccNN/ccAFv2:/files' cplaisier/ccnn
 
 # General
 from importlib.resources import path
@@ -73,14 +73,10 @@ logging.getLogger('tensorflow').disabled = True
 ################
 ## Load model ##
 ################
-os.chdir('ccAFv2_py')
 
-with path('ccAF', 'ccAFv2_model.h5') as inPath:
-    _classifier = keras.models.load_model(inPath)
-with path('ccAF', 'ccAFv2_genes.csv') as inPath:
-    _genes = list(pd.read_csv(inPath, index_col=0, header=0)['human_ensembl'])
-with path('ccAF', 'ccAFv2_classes.txt') as inPath:
-    _classes = list(pd.read_csv(inPath, header=None)[0])
+_classifier = keras.models.load_model('ccAFv2_model.h5')
+_genes = list(pd.read_csv('ccAFv2_genes.csv', index_col=0, header=0)['human_ensembl'])
+_classes = list(pd.read_csv('ccAFv2_classes.txt', header=None)[0])
 
 ###############
 ## Functions ##
@@ -184,12 +180,13 @@ def _predict_new_data(new_data, classifier):
     return classifier.predict(new_data)
 
 
-# Set up
-os.chdir('../ccNN')
+# Folder set up
 sets = ['U5']
 p_cutoffs = {'U5':0.05}
-resdir = 'data/normalized/final'
-savedir = 'results'
+resdir = 'data/'+sets[0]
+savedir = 'results/missing_genes_analysis'
+if not os.path.exists(savedir):
+    os.makedirs(savedir)
 
 # Load up conversion file
 symEnsmbl = pd.read_csv('geneConversion/U5/filtered_feature_bc_matrix/features.tsv.gz', header = None, index_col=1, sep='\t')
@@ -219,15 +216,15 @@ for i in hgncEnsmbl.loc[~hgncEnsmbl['Alias symbols'].isnull()].index:
         hgncAliasEnsmbl[j] = ensmbl
 
 #---------------------------------------------
-# Load data and subset genes to marker genes
+# Load data and subset genes to top marker genes
 #--------------------------------------------
 datasets = {}
 for set1 in sets:
+    # Load in normalized U5-hNSC data
     print('\nLoading U5-hNSC scRNA-seq data...')
     datasets[set1] = sc.read_loom(resdir+'/'+set1+'_normalized_gene_symbols.loom')
     datasets[set1].obs['new_clusters'] = datasets[set1].obs['ccAF']
     datasets[set1].obs['dataset'] = set1
-    len(datasets[set1].var_names)
     # Convert genes to Ensembl
     print('\nConverting gene symbols to Ensembl...')
     common = [i for i in datasets[set1].var_names if i in symEnsmbl[0]]
@@ -235,14 +232,15 @@ for set1 in sets:
     datasets[set1].var_names = pd.Index(symEnsmbl.loc[datasets[set1].var_names,0], name='Ensembl')
     # Subset marker genes
     print('\nSubsetting data genes to marker genes...')
-    mgenes = pd.read_csv('markerGenes/U5_scTransform_Markers_together_030423.csv', header = 0, index_col = 0)
+    mgenes = pd.read_csv('markerGenes/U5_scTransform_Markers_together.csv', header = 0, index_col = 0)
+    # Filter marker genes by avg_log2FC
     mgenes = mgenes.loc[mgenes['avg_log2FC'].ge(0.25),:]
+    # Remove G1/other cells for ccAFv2 training
     mgenes = mgenes[mgenes['cluster'] != 'G1/other']
-    mgenes = mgenes.loc[mgenes['p_val_adj'].le(0.05),:]
+    # Filter marker genes by p_val_adj
+    mgenes = mgenes.loc[mgenes['p_val_adj'].le(p_cutoffs[set1]),:]
     mgenes = list(mgenes['gene'])
-    len(mgenes)
     mgenes = list(set(mgenes))
-    len(mgenes)
     tmp1 = []
     missed = []
     for j in mgenes:
@@ -257,6 +255,7 @@ for set1 in sets:
     mg1 = tmp1
     mg1 = list(set(mg1).intersection(datasets[set1].var_names))
     print('U5 marker genes: '+str(len(mg1)))
+    # Subset data to marker genes
     datasets[set1] = datasets[set1][:,mg1]
 
 #------------------------
@@ -265,12 +264,9 @@ for set1 in sets:
 
 # Save as data as new variable
 ccAF1_scanpy = datasets[set1]
-calls = pd.read_csv('U5_ccAFv2_calls_020224.csv', index_col = 0)
+ccAFv2_calls = pd.read_csv(resdir+'/'+set1+'_ccAFv2_calls.csv', index_col = 0)
 # Set column name
-ccAF1_scanpy.obs['ccAFv2'] = calls['x']
-tmp = ccAF1_scanpy[ccAF1_scanpy.obs['ccAFv2'] != 'Unknown']
-
-#ccAF1_scanpy.obs['new_clusters'] = ccAF1_scanpy.obs['ccAFv2']
+ccAF1_scanpy.obs['ccAFv2'] = ccAFv2_calls['x']
 ccAF1_scanpy.obs['new_clusters'] = ccAF1_scanpy.obs['ccAF']
 # Parameters for CV
 nfolds = 10
@@ -283,22 +279,10 @@ GeneInds = np.arange(0, nGenes)
 ### ccAFv2 CV ###
 #################
 
-# Make folder to store downstream results
-if not os.path.exists('results/SensitivityAnalysis'):
-    os.makedirs('results/SensitivityAnalysis')
-    print ("Directory created")
-else:
-    print("Directory already exists")
-
-if not os.path.exists('results/SensitivityAnalysis/final_cutoff'):
-    os.makedirs('results/SensitivityAnalysis/final_cutoff')
-    print ("Directory created")
-else:
-    print("Directory already exists")
-
 # Cross validation within dataset
 # Percentage of genes to test for cross-validation classification
-cutoffs = [0.5, 0.7, 0.9]
+cutoffs = [0.5]
+#cutoffs = [0.5, 0.7, 0.9]
 percs = [0.99, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
 for cutoff1 in cutoffs:
     print('\nUnknown cutoff...', cutoff1)
@@ -319,26 +303,22 @@ for cutoff1 in cutoffs:
     DF = pd.concat([pd.DataFrame(list(truelab)*10).rename(columns={0:'True Labels'}), pd.DataFrame(pred)], axis=1)[['True Labels']+percs]
     #DF.replace('nan', np.nan, inplace=True)
     # Save out data
-    DF.to_csv('results/SensitivityAnalysis/final_cutoff/ccAFv2_CV_sensitivity_analysis_'+str(cutoff1)+'_031224.csv')
+    DF.to_csv(savedir+'/'+set1+'_ccAFv2_CV_missing_genes_analysis_'+str(cutoff1)+'.csv')
 
 
 # Read in data for plotting
-resdir9 = 'results/SensitivityAnalysis/final_cutoff'
-nfolds = 10
-labs = pd.read_csv('U5_ccseurat_calls_101023.csv')
-labs['x'].replace({'G2M': 'G2/M'}, inplace=True)
-extra_col = list(labs['x'])*10
-#cutoffs = [0.5, 0.7, 0.9]
+ccSeurat_calls = pd.read_csv(resdir+'/'+set1+'_ccSeurat_calls.csv')
+ccSeurat_calls['x'].replace({'G2M': 'G2/M'}, inplace=True)
+extra_col = list(ccSeurat_calls['x'])*10
 cutoffs = [0.5]
 percs = [0.99, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
-#percs = [0.99, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2] #0.9
 input = {}
 results = {}
 f1 = {}
 cells_predicted = {}
 error_rate = {}
 for cutoff1 in cutoffs:
-    file1 = pd.read_csv(resdir9+'/ccAFv2_CV_sensitivity_analysis_'+str(cutoff1)+'_031224.csv')
+    file1 = pd.read_csv(savedir+'/'+set1+'_ccAFv2_CV_missing_genes_'+str(cutoff1)+'.csv')
     file1['ccSeurat'] = extra_col
     input[cutoff1] = file1
     numSamples = len(input[cutoff1])/nfolds
@@ -378,7 +358,6 @@ col5 = []
 col6 = []
 for cutoff1 in cutoffs:
     for perc1 in percs:
-        #print(perc1)
         df = pd.merge(pd.merge(pd.DataFrame(range(nfolds), results[cutoff1][perc1]).reset_index().rename(columns = {'index':'adjusted_mutual_score'}), pd.DataFrame(range(nfolds), cells_predicted[cutoff1][perc1]).reset_index().rename(columns = {'index':'cells_predicted'}), on=0), pd.DataFrame(range(nfolds), error_rate[cutoff1][perc1]).reset_index().rename(columns = {'index':'error_rate'}), on=0)
         df['cutoff'] = cutoff1
         df['percentage_genes'] = perc1
@@ -396,7 +375,7 @@ col5 = [item for sublist in col5 for item in sublist]
 col6 = [item for sublist in col6 for item in sublist]
 df2 = pd.DataFrame([col1, col2, col3, col4, col5, col6]).T
 df2.rename(columns={0:'adjusted_mutual_score', 1:'cells_predicted', 2:'error_rate', 3: 'k', 4: 'cutoff', 5:'percentage_genes'}, inplace=True)
-df2.to_csv(resdir9+'U5_percentage_genes_remove_unknowns_'+str(cutoff1)+'_031224.csv')
+df2.to_csv(savedir+'/'+set1+'_percentage_genes_remove_unknowns_'+str(cutoff1)+'.csv')
 # Together plots
 sns.set(style="whitegrid", font_scale=3)
 fig, ax1 = plt.subplots(figsize=(40,20))
@@ -404,7 +383,7 @@ df3 = df2.melt(id_vars = ['percentage_genes', 'k', 'cutoff'], var_name='metric',
 sns.boxplot(hue = "percentage_genes", y = "value", x = "metric", data = df3, ax = ax1, palette = "husl")
 sns.move_legend(ax1, "best", bbox_to_anchor=(1,1), ncol=1)
 ax1.set(ylabel="value")
-plt.savefig(resdir9+'/U5_percentage_genes_boxplot_together_remove_unknowns_'+str(cutoff1)+'_031224.pdf')
+plt.savefig(savedir+'/'+set1+'_percentage_genes_boxplot_together_remove_unknowns_'+str(cutoff1)+'.pdf')
 plt.clf()
 
 
@@ -424,4 +403,4 @@ df3 = pd.DataFrame(
      'ami': list(median_ami.values())
     })
 df3.index = percs
-df3.to_csv(resdir9+'/U5_all_metrics_percentage_genes_sensitivity_medians_'+str(cutoff1)+'_031224.csv')
+df3.to_csv(savedir+'/U5_all_metrics_percentage_genes_sensitivity_medians_'+str(cutoff1)+'.csv')
--- scripts/24_GSE155121_missing_genes_analysis.py
+++ scripts/24_GSE155121_missing_genes_analysis.py
@@ -17,7 +17,7 @@
 ## mention who built it. Thanks. :-)                    ##
 ##########################################################
 
-#docker run -it -v '/home/soconnor/old_home:/files' cplaisier/ccnn
+#docker run -it -v '/home/soconnor/old_home/ccNN/ccAFv2:/files' cplaisier/ccnn
 
 # General
 from importlib.resources import path
@@ -73,14 +73,10 @@ logging.getLogger('tensorflow').disabled = True
 ################
 ## Load model ##
 ################
-os.chdir('ccAFv2_py')
 
-with path('ccAF', 'ccAFv2_model.h5') as inPath:
-    _classifier = keras.models.load_model(inPath)
-with path('ccAF', 'ccAFv2_genes.csv') as inPath:
-    _genes = list(pd.read_csv(inPath, index_col=0, header=0)['human_ensembl'])
-with path('ccAF', 'ccAFv2_classes.txt') as inPath:
-    _classes = list(pd.read_csv(inPath, header=None)[0])
+_classifier = keras.models.load_model('ccAFv2_model.h5')
+_genes = list(pd.read_csv('ccAFv2_genes.csv', index_col=0, header=0)['human_ensembl'])
+_classes = list(pd.read_csv('ccAFv2_classes.txt', header=None)[0])
 
 ###############
 ## Functions ##
@@ -183,12 +179,22 @@ def _predict_new_data(new_data, classifier):
 
 # Folder and tag set up
 # change directory for testing
-os.chdir('../ccNN')
 tags = ['GSE155121']
 tag = 'NSC'
 ws = ['W8-1']
 resdir = 'testData'
 resdir5 = 'results'
+
+tags = ['GSE155121']
+tag = 'NSC'
+ws = ['W8-1']
+resdir = 'data'
+savedir = 'results/missing_genes_analysis'
+if not os.path.exists(savedir):
+    os.makedirs(savedir)
+    print ("Directory created")
+else:
+    print("Directory already exists")
 
 # Common parameters
 cutoffs = [0.5]
@@ -200,12 +206,10 @@ for tag1 in tags:
     for ws1 in ws:
         print('\nLoading '+tag1+' '+ws1+' data...')
         if tag1 == 'GSE155121':
-            resdir7 = resdir+'/'+tag1+'/NSC'
-            datasets[tag1] = sc.read_h5ad(resdir+'/GSE155121/NSC/'+ws1+'_normalized_ensembl_test2.h5ad')
+            resdir2 = resdir+'/'+tag1+'/NSC'
+            datasets[tag1] = sc.read_h5ad(resdir2+'/'+ws1+'_normalized_ensembl.h5ad')
         datasets[tag1].obs['dataset'] = tag1
         datasets[tag1].obs['week_stage'] = ws1
-        print(datasets[tag1].shape)
-
 #------------------------
 # Cross validation
 #-----------------------
@@ -213,8 +217,6 @@ for tag1 in tags:
 # Save as data as new variable
 ccAF1_scanpy = datasets[tag1]
 ccAF1_scanpy.obs['new_clusters'] = ccAF1_scanpy.obs['ccAFv2']
-# Parameters for CV
-nfolds = 10
 # Initialize helper vars/indices for subsetting genes
 nCells = ccAF1_scanpy.shape[0]
 nGenes = ccAF1_scanpy.shape[1]
@@ -224,23 +226,11 @@ GeneInds = np.arange(0, nGenes)
 ### ccAFv2 CV ###
 #################
 
-# Make folder to store downstream results
-if not os.path.exists('results/SensitivityAnalysis'):
-    os.makedirs('results/SensitivityAnalysis')
-    print ("Directory created")
-else:
-    print("Directory already exists")
-
-if not os.path.exists('results/SensitivityAnalysis/final_cutoff'):
-    os.makedirs('results/SensitivityAnalysis/final_cutoff')
-    print ("Directory created")
-else:
-    print("Directory already exists")
-
 
 # Cross validation within dataset
 # Percentage of genes to test for cross-validation classification
-cutoffs = [0.5, 0.7, 0.9]
+cutoffs = [0.5]
+#cutoffs = [0.5, 0.7, 0.9]
 percs = [0.99, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
 for cutoff1 in cutoffs:
     print('\nUnknown cutoff...', cutoff1)
@@ -261,23 +251,20 @@ for cutoff1 in cutoffs:
     DF = pd.concat([pd.DataFrame(list(truelab)*10).rename(columns={0:'True Labels'}), pd.DataFrame(pred)], axis=1)[['True Labels']+percs]
     #DF.replace('nan', np.nan, inplace=True)
     # Save out data
-    DF.to_csv('results/SensitivityAnalysis/final_cutoff/'+tag1+'_ccAFv2_CV_sensitivity_analysis_'+str(cutoff1)+'_032224.csv')
+    DF.to_csv(savedir+'/'+tag1+'_'+ws1+'_ccAFv2_CV_missing_genes_analysis_'+str(cutoff1)+'.csv')
 
 
 # Read in data for plotting
-resdir9 = 'results/SensitivityAnalysis/final_cutoff'
-nfolds = 10
-labs = ccAF1_scanpy.obs['Phase']
-extra_col = list(labs)*10
+ccSeurat_calls = ccAF1_scanpy.obs['Phase']
+extra_col = list(ccSeurat_calls)*10
+cutoffs = [0.5]
 #cutoffs = [0.5, 0.7, 0.9]
-cutoffs = [0.9]
 percs = [0.99, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
-#percs = [0.99, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2] #0.9
 input = {}
 results = {}
 cells_predicted = {}
 for cutoff1 in cutoffs:
-    file1 = pd.read_csv(resdir9+'/'+tag1+'_ccAFv2_CV_sensitivity_analysis_'+str(cutoff1)+'_032224.csv')
+    file1 = pd.read_csv(savedir+'/'+tag1+'_'+ws1+'_ccAFv2_CV_missing_genes_analysis_'+str(cutoff1)+'.csv')
     file1['ccSeurat'] = extra_col
     input[cutoff1] = file1
     numSamples = len(input[cutoff1])/nfolds
@@ -323,7 +310,7 @@ col4 = [item for sublist in col4 for item in sublist]
 col5 = [item for sublist in col5 for item in sublist]
 df2 = pd.DataFrame([col1, col2, col3, col4, col5]).T
 df2.rename(columns={0:'adjusted_mutual_score', 1:'cells_predicted', 2: 'k', 3: 'cutoff', 4:'percentage_genes'}, inplace=True)
-df2.to_csv(resdir9+'/'+tag1+'_percentage_genes_remove_unknowns_'+str(cutoff1)+'_032224.csv')
+df2.to_csv(savedir+'/'+tag1+'_'+ws1+'_percentage_genes_remove_unknowns_'+str(cutoff1)+'.csv')
 # Together plots
 sns.set(style="whitegrid", font_scale=3)
 fig, ax1 = plt.subplots(figsize=(40,20))
@@ -332,7 +319,7 @@ sns.boxplot(hue = "percentage_genes", y = "value", x = "metric", data = df3, ax 
 sns.move_legend(ax1, "best", bbox_to_anchor=(1,1), ncol=1)
 ax1.set(ylabel="value")
 ax1.set_ylim(0,1)
-plt.savefig(resdir9+'/'+tag1+'_percentage_genes_boxplot_together_remove_unknowns_'+str(cutoff1)+'_032224.pdf')
+plt.savefig(savedir+'/'+tag1+'_'+ws1+'_percentage_genes_boxplot_together_remove_unknowns_'+str(cutoff1)+'.pdf')
 plt.clf()
 
 
@@ -349,4 +336,4 @@ df3 = pd.DataFrame(
      'ami': list(median_ami.values())
     })
 df3.index = percs
-df3.to_csv(resdir9+'/'+tag1+'_all_metrics_percentage_genes_sensitivity_medians_'+str(cutoff1)+'_032224.csv')
+df3.to_csv(savedir+'/'+tag1+'_'+ws1+'_all_metrics_percentage_genes_sensitivity_medians_'+str(cutoff1)+'.csv')
