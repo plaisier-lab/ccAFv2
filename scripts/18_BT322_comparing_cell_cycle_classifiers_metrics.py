5baebc0f8bcc1bb7494f8b6ee8aaa2629e22780f
Samantha O'Connor <saoconn1@asu.edu>
2024-04-10 17:02:13 -0700

    Update 18_BT322_comparing_cell_cycle_classifiers_metrics.py

--- scripts/18_BT322_comparing_cell_cycle_classifiers_metrics.py
+++ scripts/18_BT322_comparing_cell_cycle_classifiers_metrics.py
@@ -18,7 +18,7 @@
 ## mention who built it. Thanks. :-)                    ##
 ##########################################################
 
-# docker run -it -v '/home/soconnor/old_home/ccNN:/files' cplaisier/ccnn
+#docker run -it -v '/home/soconnor/old_home/ccNN/ccAFv2:/files' cplaisier/ccnn
 
 # Imports
 import pandas as pd
@@ -41,20 +41,20 @@ from scipy import stats
 ##################
 
 ## Load datasets and concatenate
-tags = ['ccafv2', 'tricycle', 'recat', 'ccschwabe', 'peco', 'cyclone']
-resdir = 'cross_validation'
+data1 = 'BT322'
+tags = ['ccafv2', 'tricycle', 'recat', 'ccschwabe', 'peco', 'cyclone', 'ccaf']
+resdir = 'compare_classifiers'
 datasets = {}
 for tag1 in tags:
-    datasets[tag1] = pd.read_csv(resdir+'/BT322/'+tag1+'/CV_classification_report_020824_with_Phase_as_ref.csv', index_col = 0)
+    datasets[tag1] = pd.read_csv(resdir+'/'+data1+'/'+tag1+'_CV_classification_report_with_Phase_as_ref.csv', index_col = 0)
     datasets[tag1]['classifier'] = tag1
     datasets[tag1].rename(columns = {datasets[tag1][datasets[tag1].columns[0]].name: 'truelab'}, inplace=True)
     datasets[tag1].rename(columns = {datasets[tag1][datasets[tag1].columns[1]].name: 'predlab'}, inplace=True)
 
-df_all = pd.concat([datasets[tags[0]], datasets[tags[1]], datasets[tags[2]], datasets[tags[3]], datasets[tags[4]], datasets[tags[5]]], axis = 0)
+df_all = pd.concat([datasets[tags[0]], datasets[tags[1]], datasets[tags[2]], datasets[tags[3]], datasets[tags[4]], datasets[tags[5]], datasets[tags[6]]], axis = 0)
 # change Unknown to NaN
 df_all['predlab'].replace({'Unknown': np.nan, 'G2M': 'G2/M','G2.M':'G2/M', 'M.G1': 'M/Early G1'}, inplace=True)
 #df_all.dropna(inplace = True)
-
 
 # Tabulate AMIs
 ami_scores = {}
@@ -67,135 +67,21 @@ for tag1 in tags:
 lst = [7, 4, 6, 5, 4, 3, 8]
 x = np.array(lst)
 y = np.array(list(ami_scores.values()))
-y = list(y)
-y.append(ccaf_ami)
-
-m = (y[1]-y[2])/(x[1]-x[2])
-b = y[1]-(m*x[1])
-
-descrip = np.array(['ccAFv2', 'tricycle', 'recat', 'ccSchwabe', 'peco', 'cyclone', 'ccAF'])
+descrip = np.array(tags)
 df2 = pd.DataFrame(y, descrip)
 df2.rename(columns={0:'AMI'}, inplace=True)
-df2.to_csv(resdir+'/BT322_AMI_seurat_reference.csv')
+df2.to_csv(resdir+'/'+data1+'/'+data1+'_AMI_seurat_reference.csv')
 
 # Plot
 fig, ax = plt.subplots()
-fig, ax = plt.subplots(figsize=(6,6))
+fig, ax = plt.subplots(figsize=(10,8))
 ax.errorbar(x, y,
-            #xerr=xerr1,
-            #yerr=yerr1,
             fmt='o', ecolor = 'black')
 ax.set_xlabel('# of classes')
 ax.set_ylabel('Adjusted Mutual Information Score')
-ax.axline((0, b), slope=m, color='C0', label='by slope')
 ax.set_ylim(0,1)
 ax.set_xlim(0,8)
 for i, txt in enumerate(descrip):
     ax.annotate(txt, (x[i], y[i]))
-#matplotlib.pyplot.errorbar(np.array(x),np.array(y), np.array(xerr), np.array(yerr), 'o')
-#plt.errorbar(x, y, yerr=yerr1, fmt='o')
-plt.savefig(resdir+'/BT322_AMI_no_classes_together_seurat_ref_021424.pdf')
+plt.savefig(resdir+'/'+data1+'/'+data1+'_AMI_number_classes_seurat_ref.pdf')
 plt.clf()
-
-
-
-
-
-
-
-# long way
-"""
-# Run comparison tests
-results = {}
-cells_predicted = {}
-defined_cell_states = df_all['truelab']
-compare_cell_states = df_all['predlab']
-numSamples = round(2962*0.8) # removed G1/other cells
-nfolds = 10
-for tag1 in tags:
-    results[tag1] = []
-    cells_predicted[tag1] = []
-    defined_cell_states = df_all[df_all['classifier'] == tag1]['truelab']
-    compare_cell_states = df_all[df_all['classifier'] == tag1]['predlab']
-    for k in range(nfolds):
-        bind = int(numSamples*k)
-        eind = int(numSamples*(k+1))
-        truelab = defined_cell_states.iloc[bind:eind]
-        #truelab = compare_cell_states.iloc[bind:eind]
-        predlab = compare_cell_states.iloc[bind:eind]
-        # Adjusted mutual score; drop unknowns
-        results[tag1].append(adjusted_mutual_info_score(truelab[predlab.dropna().index], predlab.dropna()))
-        # Cells predicted
-        cells_predicted[tag1].append(1 - sum(predlab.isna())/len(predlab))
-
-col1 = []
-col2 = []
-col3 = []
-for tag1 in tags:
-    tmp1 = pd.DataFrame(range(nfolds), results[tag1]).reset_index().rename(columns = {'index':'adjusted_mutual_score'})
-    tmp2 = pd.DataFrame(range(nfolds), cells_predicted[tag1]).reset_index().rename(columns = {'index':'cells_predicted'})
-    df = pd.concat([tmp1['adjusted_mutual_score'], tmp2['cells_predicted']], axis=1)
-    df['classifier'] = tag1
-    df.reset_index(inplace=True)
-    col1.append(list(df['adjusted_mutual_score']))
-    col2.append(list(df['cells_predicted']))
-    col3.append(list(df['classifier']))
-
-col1 = [item for sublist in col1 for item in sublist]
-col2 = [item for sublist in col2 for item in sublist]
-col3 = [item for sublist in col3 for item in sublist]
-
-df2 = pd.DataFrame([col1, col2, col3]).T
-df2.rename(columns={0:'adjusted_mutual_score', 1:'cells_predicted', 2: 'classifier'}, inplace=True)
-#df2['data'] = data1
-for metric1 in ['adjusted_mutual_score', 'cells_predicted']:
-    sns.set(style="whitegrid", font_scale=3)
-    fig, ax1 = plt.subplots(figsize=(40,20))
-    sns.boxplot(hue = 'classifier', y = metric1, x = "data", data = df2, ax = ax1, palette = "husl")
-    ax1.set(ylabel=metric1)
-    ax1.set_ylim(0,1)
-    #plt.savefig(resdir+'/'+data1+'_'+metric1+'_classifier_comparison_111623.pdf')
-    #plt.savefig(resdir+'/'+data1+'_'+metric1+'_classifier_comparison_010424.pdf')
-    plt.savefig(resdir+'/'+data1+'_'+metric1+'_classifier_comparison_020524.pdf')
-    plt.clf()
-
-ccaf_pred = pd.read_csv(resdir+'/BT322/ccaf/CV_classification_report_020824_with_Phase_as_ref.csv', index_col = 0)
-ccaf_pred['classifier'] = tag1
-ccaf_pred.rename(columns = {ccaf_pred[ccaf_pred.columns[0]].name: 'truelab'}, inplace=True)
-ccaf_pred.rename(columns = {ccaf_pred[ccaf_pred.columns[1]].name: 'predlab'}, inplace=True)
-truelab = ccaf_pred['truelab']
-predlab = ccaf_pred['predlab']
-ccaf_ami = adjusted_mutual_info_score(truelab, predlab)
-
-
-median_ami = []
-median_cp = []
-for classifier1 in list(df2['classifier'].unique()):
-    median_ami.append(np.median(df2[df2['classifier'] == classifier1]['adjusted_mutual_score']))
-    median_cp.append(np.median(df2[df2['classifier'] == classifier1]['cells_predicted']))
-
-median_cp_perc = [i * 100 for i in median_cp]
-
-x = np.array(median_cp_perc)
-y = np.array(median_ami)
-#xerr1 = np.array([0.001, 0, 0.002, 0, 0.005, 0.003, 0])
-#yerr1 = np.array([0.005, 0.005, 0.006, 0.006, 0.004, 0.004, 0.004])
-#descrip = np.array(['ccAF', 'ccAFv2', 'ccSeurat', 'tricycle', 'recat', 'ccSchwabe', 'peco', 'cyclone'])
-descrip = np.array(['ccAFv2', 'tricycle', 'recat', 'ccSchwabe', 'peco', 'cyclone'])
-fig, ax = plt.subplots()
-fig, ax = plt.subplots(figsize=(10,10))
-ax.errorbar(x, y,
-            #xerr=xerr1,
-            #yerr=yerr1,
-            fmt='o', ecolor = 'black')
-ax.set_xlabel('Cells Predicted (%)')
-ax.set_ylabel('Adjusted Mutual Information Score')
-ax.set_ylim(0,1.1)
-ax.set_xlim(50,100)
-for i, txt in enumerate(descrip):
-    ax.annotate(txt, (x[i], y[i]))
-#matplotlib.pyplot.errorbar(np.array(x),np.array(y), np.array(xerr), np.array(yerr), 'o')
-#plt.errorbar(x, y, yerr=yerr1, fmt='o')
-plt.savefig(resdir+'/BT322/AMI_cellsPredicted_together_020824.pdf')
-plt.clf()
-"""
